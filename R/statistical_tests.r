036296b6746d65fbd0303847b2247a0459d702da
Jonathan Barenboim <jon.barenboim@gmail.com>
2017-12-06 14:49:16 -0500

    Optimized orderedHypergeometric method; also fixes a bug that gave slightly off p-values for some cases

--- R/statistical_tests.r
+++ R/statistical_tests.r
@@ -1,27 +1,25 @@
-#' Hypergeometric Test
+#' Hypergemoetric Test
 #'
-#' Perform a Hypergeometric, aka Fisher's exact test, on a list of significant
-#' genes and annotation genes
+#' Perform a Hypergeometric test, aka Fisher's exact test, on a 2x2 contingency
+#' table with a 'greater' alternative hypothesis. That is, find the
+#' probability that counts[1, 1] or more genes would be found in annotations,
+#' assuming the null hypothesis.
 #'
-#' @param complement character vector of gene names. All other genes not being
-#'   tested for enrichment. Ie \code{setdiff(background, genelist)}
-#'
-#' @inheritParams orderedHypergeometric
+#' @param counts a 2x2 numerical matrix representing a contingency table
 #'
 #' @return a p-value
 #'
 #' @examples
-#' \dontrun{
-#'   hypergeometric(c('HERC2', 'SP100'), c('PHC2', 'BLM', 'NDC1', 'XPC', 'SMC3'), c('HERC2', 'PHC2', 'BLM'))
-#' }
-hypergeometric <- function(genelist, complement, annotations) {
-    genelist1 <- length(which(genelist %in% annotations))
-    genelist0 <- length(genelist) - genelist1
-    complement1 <- length(which(complement %in% annotations))
-    complement0 <- length(complement) - complement1
-    counts <- matrix(data=c(genelist1, genelist0, complement1, complement0), nrow=2)
-    fisher.test(counts, alternative='greater')
+#' hypergeometric(matrix(data=c(1, 8, 14, 836), nrow=2))
+#'
+hypergeometric <- function(counts) {
+    m <- counts[1, 1] + counts[2, 1]
+    n <- counts[1, 2] + counts[2, 2]
+    k <- counts[1, 1] + counts[1, 2]
+    x <- counts[1, 1]
+    phyper(x-1, m, n, k, lower.tail=FALSE)
 }
+
 
 #' Ordered Hypergeometric Test
 #'
@@ -47,29 +45,41 @@ hypergeometric <- function(genelist, complement, annotations) {
 #'
 #' @examples
 #' \dontrun{
-#'    orderedHypergeometric(c('HERC2', 'SP100'), c('PHC2', 'BLM', 'NDC1', 'XPC', 'SMC3', 'HERC2', 'SP100'),
+#'    orderedHypergeometric(c('HERC2', 'SP100'), c('PHC2', 'BLM', 'XPC', 'SMC3', 'HERC2', 'SP100'),
 #'                          c('HERC2', 'PHC2', 'BLM'))
 #' }
 orderedHypergeometric <- function(genelist, background, annotations) {
-    # remove genes in genelist from background to form a list of all other genes
-    # genes that are not being tested in each call will be added back
-    complement <- setdiff(background, genelist)
-
-    # Test only when a gene is in annotation as that's the only case that
-    # could reduce the p-value
+    # Only test subsets of genelist that end with a gene in annotations since
+    # these are the only tests for which the p-value can decrease
     which.in <- which(genelist %in% annotations)
-
     if (length(which.in) == 0) return(list(p.val=1, ind=1))
 
-    # Test first i genes in genelist. The rest of genelist has to be added to
-    # complement
-    f <- function(i) {
-        hypergeometric(genelist[1:i],
-                       c(genelist[(i+1):length(genelist)], complement),
-                       annotations)
-    }
-    scores <- sapply(which.in, function(i) f(i)$p.val)
+    # Construct the counts matrix for the first which.in[1] genes
+    gl <- genelist[1:which.in[1]]
+    cl <- setdiff(background, gl)
+    genelist0 <- length(gl) - 1
+    complement1 <- length(which(cl %in% annotations))
+    complement0 <- length(cl) - complement1
+    counts <- matrix(data=c(1, genelist0, complement1, complement0), nrow=2)
+    scores <- hypergeometric(counts)
 
+    if (length(which.in) == 1) return(list(p.val=scores, ind=1))
+
+    # Update counts and recalculate score for the rest of the indeces in which.in
+    # The genes in genelist[which.in[i]:which.in[i-1]] are added to the genes
+    # being tested and removed from the complement. Of these, 1 will always be
+    # in annotations and the rest will not. Therefore we can just modify the
+    # contingency table rather than recounting which genes are in annotations
+    for (i in 2:length(which.in)) {
+        diff <- which.in[i] - which.in[i-1]
+        counts[1, 1] <- i
+        counts[2, 1] <- counts[2, 1] + diff - 1
+        counts[1, 2] <- counts[1, 2] - 1
+        counts[2, 2] <- counts[2, 2] - diff + 1
+        scores[i] <- hypergeometric(counts)
+    }
+
+    # Return the lowest p-value and the associated index
     min.score <- min(scores)
     list(p.val=min.score, ind=which.in[max(which(scores==min.score))])
 }

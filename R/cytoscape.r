d275df14943a1f218be30638b0e6b848ddf506a1
helen-zhu <helen.m.zhu@gmail.com>
2018-11-20 19:59:19 -0500

    Save legend

--- R/cytoscape.r
+++ R/cytoscape.r
@@ -27,33 +27,46 @@
 #' @return None
 
 prepareCytoscape <- function(terms, gmt, filenames, col.significance) {
-    if (!is.null(col.significance)) {
-		tests <- unique(unlist(col.significance$evidence))
-        rows <- 1:nrow(col.significance)
-        
-        evidence.columns = do.call(rbind, lapply(col.significance$evidence, 
-        	function(x) 0+(tests %in% x)))
-        colnames(evidence.columns) = tests
-        
-        col.significance = cbind(col.significance[,"term.id"], evidence.columns)
-
-        # Use pichart
-        col.colors <- paste(rainbow(length(tests)), collapse=',')
-        instruct.str <- paste('piechart:',
-                              ' attributelist="', 
-                              paste(tests, collapse=','),
-                              '" colorlist="', 
-                              paste(col.colors, collapse=','), 
-                              '" showlabels=FALSE', sep='')
-        col.significance[, instruct := instruct.str]
-
-        write.table(terms, file=filenames[1], row.names=FALSE, sep="\t", quote=FALSE)
-        write.table(col.significance, file=filenames[2], row.names=FALSE, sep="\t", quote=FALSE)
-        write.GMT(gmt, filenames[3])
-    } else {
-        write.table(terms, file=filenames[1], row.names=FALSE, sep="\t", quote=FALSE)
-        write.GMT(gmt, filenames[2])
-    }
+  if (!is.null(col.significance)) {
+    tests <- unique(unlist(col.significance$evidence))
+    rows <- 1:nrow(col.significance)
+    
+    evidence.columns = do.call(rbind, lapply(col.significance$evidence,
+                                             function(x) 0+(tests %in% x)))
+    colnames(evidence.columns) = tests
+    
+    col.significance = cbind(col.significance[,"term.id"], evidence.columns)
+    
+    # Use pichart
+    col.colors <- paste(rainbow(length(tests)), collapse=',')
+    instruct.str <- paste('piechart:',
+                          ' attributelist="', 
+                          paste(tests, collapse=','),
+                          '" colorlist="', 
+                          paste(col.colors, collapse=','), 
+                          '" showlabels=FALSE', sep='')
+    col.significance[, instruct := instruct.str]
+    
+    # Writing the Files
+    write.table(terms, file=filenames[1], row.names=FALSE, sep="\t", quote=FALSE)
+    write.table(col.significance, file=filenames[2], row.names=FALSE, sep="\t", quote=FALSE)
+    write.GMT(gmt, filenames[3])
+    
+    # Making a Legend
+    require(ggplot2)
+    dummy_plot = ggplot(data.frame(test, 1), aes(test, fill = test)) +
+      geom_bar() +
+      scale_fill_manual(name = "Contribution", values=col.colors)
+    dummy_table = ggplot_gtable(ggplot_build(dummy_plot))
+    legend = dummy_table$grobs[[which(sapply(tmp$grobs, function(x) x$name) == "guide-box")]]
+    legend_height = ifelse(length(test) > 19, 5, length(test)*0.2 +1)
+    legend_width = ifelse(length(test) > 20, ceiling(length(test)/20)*(max(nchar(default_test))*0.05+1), max(nchar(default_test))*0.05+1)
+    ggsave(legend, filename = filenames[4], height = legend_height, width = legend_width, scale = 1)
+    
+  } else {
+    write.table(terms, file=filenames[1], row.names=FALSE, sep="\t", quote=FALSE)
+    write.GMT(gmt, filenames[2])
+  }
 }
 
 

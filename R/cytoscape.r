2232c4c0a8fcbb356cf4ae40e232007718780537
Helen <helen@Helens-iMac.local>
2018-11-23 01:38:50 -0500

    added file.dir and file.tag

--- R/cytoscape.r
+++ R/cytoscape.r
@@ -1,26 +1,27 @@
 #' Prepare files for building an Enrichment Map in Cytoscape
 #'
-#' This function writes three files that are used to build an network using
+#' This function writes four files that are used to build an network using
 #' Cytoscape and the EnrichmentMap app.
 #'   The four files written are:
 #'   \describe{
-#'     \item{cytoscape.filenames[1]}{A list of significant terms and the
-#'       associated adjusted p-value. Only terms with \code{adjusted.p.val <= significant} are
-#'       written to this file}
-#'     \item{cytoscape.filenames[2]}{A matrix of column contributions to each
-#'       term. A \code{1} indicates that that term is significant using only
-#'       that column to test for enrichment analysis}. Left out if
-#'       contribution==FALSE and therefore col.significance==NULL
-#'     \item{cytoscape.filenames[3]}{A Shortened version of the supplied gmt
-#'       file, containing only the terms in \code{cytoscape.filenames[1]}}
-#'     \item{cytoscape.filenames[4]}{A legend with colours matching contributions
+#'     \item{\code{cytoscape.file.tag}_pathways.txt}{A list of significant terms and the
+#'     associated p-value. Only terms with \code{adjusted.p.val <= significant} are
+#'     written to this file}
+#'     \item{\code{cytoscape.file.tag}_subgroups.txt}{A matrix indicating whether the significant
+#'     pathways are found to be significant when considering only one column from
+#'     \code{scores}. A 1 indicates that that term is significant using only that
+#'     column to test for enrichment analysis}
+#'     \item{\code{cytoscape.file.tag}.gmt}{A Shortened version of the supplied gmt
+#'     file, containing only the terms in \code{cytoscape.file.tag}_pathways.txt}
+#'     \item{\code{cytoscape.file.tag}_legend.pdf}{A legend with colours matching contributions
 #'     from columns in \code{scores}}
 #'   }
 #'
 #' @param terms a data.table with columns 'term.id', 'term.name', 'adjusted.p.val'
 #' @param gmt an abridged gmt object containing only the pathways that were
 #' found to be significant
-#' @param filenames vector of 3 or 4 filesnames denoting where to write files to
+#' @param file_tag user defined tag used to identify content
+#' @param file_dir user defined directory to write output files
 #' @param col.significance a data.table with a column 'term.id' and a column
 #' for each test indicating whether a pathway is signficiant (TRUE) or not
 #' (FALSE) when considering only that column. If contribution==TRUE, use
@@ -29,7 +30,7 @@
 #'
 #' @return None
 
-prepareCytoscape <- function(terms, gmt, filenames, col.significance) {
+prepareCytoscape <- function(terms, gmt, file_tag, file_dir, col.significance) {
   if (!is.null(col.significance)) {
     tests <- unique(unlist(col.significance$evidence))
     rows <- 1:nrow(col.significance)
@@ -51,9 +52,18 @@ prepareCytoscape <- function(terms, gmt, filenames, col.significance) {
     col.significance[, instruct := instruct.str]
     
     # Writing the Files
-    write.table(terms, file=filenames[1], row.names=FALSE, sep="\t", quote=FALSE)
-    write.table(col.significance, file=filenames[2], row.names=FALSE, sep="\t", quote=FALSE)
-    write.GMT(gmt, filenames[3])
+    write.table(terms, 
+                file=paste0(file_dir, file_tag, "_pathways.txt"), 
+                row.names=FALSE, 
+                sep="\t", 
+                quote=FALSE)
+    write.table(col.significance, 
+                file=paste0(file_dir, file_tag, "_subgroups.txt"), 
+                row.names=FALSE, 
+                sep="\t", 
+                quote=FALSE)
+    write.GMT(gmt, 
+              paste0(file_dir, file_tag,".gmt"))
     
     # Making a Legend
     if (requireNamespace("ggplot2", quietly = TRUE)) {
@@ -76,7 +86,7 @@ prepareCytoscape <- function(terms, gmt, filenames, col.significance) {
                             max(nchar(tests))*0.05+1)
       ggsave(legend,
              device = "pdf",
-             filename = filenames[4], 
+             filename = paste0(file_dir, file_tag, "_legend.pdf"), 
              height = legend_height, 
              width = legend_width, 
              scale = 1)
@@ -87,8 +97,13 @@ prepareCytoscape <- function(terms, gmt, filenames, col.significance) {
     
     
   } else {
-    write.table(terms, file=filenames[1], row.names=FALSE, sep="\t", quote=FALSE)
-    write.GMT(gmt, filenames[2])
+    write.table(terms, 
+                file=paste0(file_dir, file_tag, "_pathways.txt"),
+                row.names=FALSE, 
+                sep="\t", 
+                quote=FALSE)
+    write.GMT(gmt, 
+              paste0(file_dir, file_tag,".gmt"))
   }
 }
 

d103ad4ce96b8421086b216b8e7dbd8fc9f083a6
MSlobodyanyuk <michael.slobody@gmail.com>
2022-12-06 19:13:15 -0500

    Updated variable names

--- tests/testthat/helper.r
+++ tests/testthat/helper.r
@@ -1,6 +1,6 @@
 # Prepare testing data
 gmt <- read.GMT('test.gmt')
-gmt_hox <- read.GMT('hsapiens_REAC_subset.gmt')
+gmt_reac <- read.GMT('hsapiens_REAC_subset.gmt')
 dat <- as.matrix(read.table('test_data.txt', header=TRUE, row.names='Gene'))
 dat[is.na(dat)] <- 1
 background <- makeBackground(gmt)
@@ -9,22 +9,22 @@ background <- makeBackground(gmt)
 CStag = "CS_files"
 
 # Prepare testing data for scores_direction and expected_direction
-df_hox <- read.table('test_hoxa10as_data.tsv', header = TRUE, sep = '\t')
-hox_score <- data.frame(row.names = df_hox[,1], overexpression = df_hox[,3], knockdown = df_hox[,5])
-hox_score <- as.matrix(hox_score)
-hox_score[is.na(hox_score)] <- 1
-hox_direction <- data.frame(row.names = df_hox[,1], overexpression = df_hox[,2], knockdown = df_hox[,4])
-hox_direction <- as.matrix(hox_direction)
-hox_direction[is.na(hox_direction)] <- 1
-hox_expected_direction <- c(-1,1)
+df <- read.table('test_data_rna_protein.tsv', header = TRUE, row.names = "gene", sep = '\t')
+scores_test <- data.frame(row.names = rownames(df), rna = df[,1], protein = df[,3])
+scores_test <- as.matrix(scores_test)
+scores_test[is.na(scores_test)] <- 1
+direction_test <- data.frame(row.names = rownames(df), rna = df[,2], protein = df[,4])
+direction_test <- as.matrix(direction_test)
+direction_test[is.na(direction_test)] <- 1
+expected_direction_test <- c(1,1)
 
 # Run ActivePathways quickly
 run_ap_short <- function(dat) ActivePathways(dat[,1, drop = F], gmt[1:3], cutoff=1, significant=1)
 run_ap_short_contribution <- function(dat) ActivePathways(dat, gmt[1:3], cutoff=1, significant=1)
-run_ap <- function(hox_score,hox_direction,hox_expected_direction) ActivePathways(scores = hox_score,
-                                                                                  gmt_hox, cutoff=1, significant=1,
-                                                                                  scores_direction = hox_direction,
-                                                                                  expected_direction = hox_expected_direction)
+run_ap <- function(scores_test,direction_test,expected_direction_test) ActivePathways(scores = scores_test,
+                                                                                  gmt_reac, cutoff=1, significant=1,
+                                                                                  scores_direction = direction_test,
+                                                                                  expected_direction = expected_direction_test)
 
 
 

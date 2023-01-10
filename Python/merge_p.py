ac17c4dfe719e2bad8d6c4b5b2d6c8894a398624
abahcheli <abahchel@uwo.ca>
2023-01-10 17:58:37 -0500

    merge_p rough final

--- Python/merge_p.py
+++ Python/merge_p.py
@@ -2,14 +2,17 @@ import pandas as pd
 import numpy as np
 
 import scipy.stats as stats
+from statsmodels.distributions.empirical_distribution import ECDF
 
 # can accept: numpy array, pandas data frame, or list
 def merge_p_values(scores, method='Fisher', scores_direction = None, expected_direction = None):
     # validate types of scores variable
     if type(scores) == list:
         scores = np.array(scores)
+    elif type(scores) == pd.Series:
+        scores = scores.to_numpy()
     if type(scores) != pd.DataFrame or type(scores) != np.ndarray:
-        print("scores must be a pandas data frame, numpy array, or list")
+        print("scores must be a pandas data frame, series, numpy array, or list")
         exit()
     if np.any(pd.isnull(scores)):
         print("Scores may not contain missing values")
@@ -28,10 +31,49 @@ def merge_p_values(scores, method='Fisher', scores_direction = None, expected_di
     if method not in ["Fisher", "Brown", "Stouffer", "Strube"]:
         print("Only Fisher's, Brown's, Stouffer's and Strube's methods are currently supported")
         exit()
-    # if only some p-values are directional, expected_direction must be a pandas dataframe or series
-    if type(scores_direction) != pd.DataFrame:
-        print("scores_direction must be a pandas Dataframe column labels matching the column names of the p-values dataframe or numpy array")
+    
+    # check scores_direction and expected_direction
+    if np.logical_xor(scores_direction == None, expected_direction == None):
+        print("Both scores_direction and expected_direction must be provided")
         exit()
+    
+    if scores_direction != None:
+        # if only some p-values are directional, expected_direction must be a pandas dataframe or series
+        if type(scores) != pd.Series or type(scores) != pd.DataFrame:
+            print("Scores must be a pandas series or dataframe to apply directional penalty")
+            exit()
+
+        # check expected direction
+        if type(expected_direction) == list:
+            expected_direction = np.array(expected_direction)
+        if type(expected_direction) != np.array or type(expected_direction) != pd.Series or np.any(np.abs(expected_direction) != 1):
+            print("expected_direction must be a numeric type array composed of only -1's or 1's")
+            exit()
+
+        # check scores_direction
+        if type(scores_direction) != pd.DataFrame:
+            print("scores_direction must be a pandas Dataframe column labels matching the column names of the p-values dataframe or numpy array")
+            exit()
+        if np.any(scores_direction.isnull()):
+            print("scores_direction must not contain any missing values. Fill NA's with 1 before processing")
+            exit()
+        try:
+            scores_direction = scores_direction.astype('float')
+        except:
+            print("scores_direction must be numeric")
+            exit()
+        if np.any(~np.isin(scores_direction.index, scores.index)) or np.any(~np.isin(scores_direction.columns, scores.columns)):
+            print("scores_direction index and columns must match scores index and columns")
+            exit()
+        if scores_direction.shape[1] < 2:
+            print("A minimum of two datasets from the scores matrix should have corresponding directionality data in scores_direction. Ensure column names are identical")
+            exit()
+
+        # check both
+        if scores_direction.shape[1] != len(expected_direction):
+            print("expected_direction should have the same number of entries as columns in scores_direction")
+        
+    
 
     # convert zeroes to smallest available floats
     scores[scores == 0] = 1e-300
@@ -43,11 +85,11 @@ def merge_p_values(scores, method='Fisher', scores_direction = None, expected_di
             exit()
         
         if method == 'Fisher':
-            p_val = 1 - stats.chi2.cdf((fishersMethod(scores, scores_direction, expected_direction), 2*len(scores)))
+            p_val = 1 - stats.chi2.cdf((fishers_method(scores, scores_direction, expected_direction), 2*len(scores)))
             return p_val
         
         if method == 'Stouffer':
-            p_val = 2 * (1 - stats.norm.cdf(abs(stouffersMethod(scores, scores_direction, expected_direction))))
+            p_val = 2 * (1 - stats.norm.cdf(abs(stouffers_method(scores, scores_direction, expected_direction))))
             return p_val
 
     # if there is only one column, just return those p-values
@@ -56,52 +98,52 @@ def merge_p_values(scores, method='Fisher', scores_direction = None, expected_di
 
     # if scores is a matrix with multiple columns, apply the following methods
     if method == 'Fisher':
-        p_val = np.array(list(map(lambda x: 1 - stats.chi2.cdf((fishersMethod(scores.loc[x,:], scores_direction.loc[x,:], expected_direction), 2*len(scores))), scores.index.to_numpy())))
+        p_val = np.array(list(map(lambda x: 1 - stats.chi2.cdf((fishers_method(scores.loc[x,:], scores_direction.loc[x,:], expected_direction), 2*len(scores))), scores.index)))
 
         # return dataframe
         return pd.DataFrame(p_val, index = scores.index)
 
     if method == 'Brown':
         cov_matrix = calculate_covariances(scores.transpose())
-        p_val = brownsMethod(scores, scores_direction, expected_direction, cov_matrix = cov_matrix)
+        p_val = browns_method(scores, scores_direction, expected_direction, cov_matrix = cov_matrix)
 
         return pd.DataFrame(p_val, index = scores.index)
 
     if method == 'Stouffer':
         # NOTE subset scores_direction and expected_direction
-        p_val = np.array(list(map(lambda x: 2 * (1 - stats.norm.cdf(abs(stouffersMethod(scores.loc[x,:].to_numpy(), scores_direction.loc[x,:], expected_direction)))), scores.index.to_numpy())))
+        p_val = np.array(list(map(lambda x: 2 * (1 - stats.norm.cdf(abs(stouffers_method(scores.loc[x,:].to_numpy(), scores_direction.loc[x,:], expected_direction)))), scores.index)))
 
         # return dataframe
         return pd.DataFrame(p_val, index = scores.index)
 
     if method == 'Strube':
-        p_val = brownsMethod(scores, scores_direction, expected_direction)
+        p_val = strubes_method(scores, scores_direction, expected_direction)
 
         return pd.DataFrame(p_val, index = scores.index)
 
 
-
-
-def fishersMethod(p_values, scores_direction = None, expected_direction = None):
-    if scores_direction != None and expected_direction != None:
+def fishers_method(p_values, scores_direction = None, expected_direction = None):
+    if scores_direction != None:
         # apply directionality penalty where applicable
-        directionality = expected_direction[direction_mask] * scores_direction[direction_mask] / np.abs(scores_direction[direction_mask])
+        directionality = expected_direction * scores_direction / np.abs(scores_direction)
+
+        direction_mask = np.isin(p_values.index, scores_direction.index)
         p_values_directional = p_values[direction_mask]
 
-        chisq_directional = np.abs(-2 * sum(np.log(p_values_directional) * directionality))
+        chisq_directional = np.abs(-2 * np.sum(np.log(p_values_directional) * directionality))
 
         # calculate for non-directionality p-values
         if np.any(~direction_mask):
             p_values_nondirectional = p_values[~direction_mask]
             chisq_directional = np.concatenate([np.abs(-2 * sum(np.log(p_values_nondirectional))), chisq_directional])
 
-        return np.sum(chisq_directional)
-
     else:
-        return (-2 * np.log(p_values))
+        chisq_directional = -2 * np.log(p_values)
+
+    return np.sum(chisq_directional)
 
 
-def brownsMethod(p_values, scores_direction = None, expected_direction = None, data_matrix=None, cov_matrix=None):
+def browns_method(p_values, scores_direction = None, expected_direction = None, data_matrix=None, cov_matrix=None):
     # what has been provided
     if data_matrix == None and cov_matrix == None:
         print("Either data_matrix or cov_matrix must be supplied")
@@ -109,7 +151,7 @@ def brownsMethod(p_values, scores_direction = None, expected_direction = None, d
     if data_matrix != None and cov_matrix != None:
         print("Both data_matrix and cov_matrix were supplied. Ignoring data_matrix")
     if cov_matrix == None:
-        cov_matrix = calculateCovariances(data_matrix)
+        cov_matrix = calculate_covariances(data_matrix)
 
     n_datasets = len(cov_matrix.shape[1])
     expected = 2 * n_datasets
@@ -123,10 +165,79 @@ def brownsMethod(p_values, scores_direction = None, expected_direction = None, d
         sf = 1
 
     try: 
-        p_val = np.array(list(map(lambda x: 1 - stats.chi2.cdf((fishersMethod(p_values.loc[x,:], scores_direction.loc[x,:], expected_direction), 2*len(p_values))), p_values.index.to_numpy())))
+        p_val = np.array(list(map(lambda x: 1 - stats.chi2.cdf((fishers_method(p_values.loc[x,:], scores_direction.loc[x,:], expected_direction), 2*len(p_values))), p_values.index.to_numpy())))
     except:
-        p_val = np.array(list(map(lambda x: 1 - stats.chi2.cdf((fishersMethod(p_values.loc[x,:]), 2*len(p_values))), p_values.index.to_numpy())))
+        p_val = np.array(list(map(lambda x: 1 - stats.chi2.cdf((fishers_method(p_values.loc[x,:]), 2*len(p_values))), p_values.index.to_numpy())))
 
     p_brown = 1 - stats.chi2.cdf(p_val / sf, df)
+
     return p_brown
+
+
+def stouffers_method(p_values, scores_direction = None, expected_direction = None):
+    k = len(p_values)
+
+    if scores_direction != None:
+        # apply directionality penalty where applicable
+        directionality = expected_direction * scores_direction / np.abs(scores_direction)
+
+        direction_mask = np.isin(p_values.index, scores_direction.index)
+        p_values_directional = p_values[direction_mask]
+
+        z_directional = np.abs(sum(stats.norm.ppf(p_values_directional / 2) * directionality))
+
+        # calculate for non-directionality p-values
+        if np.any(~direction_mask):
+            p_values_nondirectional = p_values[~direction_mask]
+            z_directional = np.concatenate([np.abs(np.sum(stats.norm.ppf(p_values_nondirectional / 2))), z_directional])
+
+    else:
+        z_directional = stats.norm.ppf(p_values / 2)
+
+    return np.sum(z_directional) / np.sqrt(k)
+
+
+def strubes_method(p_values, scores_direction = None, expected_direction = None):
+    # acquire the unadjusted z-value from stouffer's method
+    try:
+        stouffers_z_arr = np.array(list(map(lambda x: stouffers_method(p_values.loc[x,:], scores_direction.loc[x,:], expected_direction), p_values.index)))
+    except:
+        stouffers_z_arr = np.array(list(map(lambda x: stouffers_method(p_values.loc[x,:]), p_values.index)))
+
+    k = p_values.shape[1]
+
+    # correlation matrix
+    cor_mtx = np.abs(p_values.corr())
+    cor_mtx.loc[cor_mtx.isna()] = 0
+
+    # adjust p-value
+    adjusted_z = stouffers_z_arr * np.sqrt(k) / np.sqrt(np.sum(cor_mtx))
+    p_strube = 2 * (1 - stats.norm.cdf(abs(adjusted_z)))
+
+    return p_strube
+
+
+def transform_data(data):
+    # If all values in dat are the same (equal to y), return dat. The covariance matrix will be the zero matrix, and brown's method gives the p-value as y. Otherwise (dat - dmv) / dvsd is NaN and ecdf throws an error
+    if np.all(data == data[0]):
+        return data
+
+    # NOTE no idea what's going on here copied directly from R
+    def pop_sd(x):
+        x = np.nanvar(x) * (len(x) - 1) / len(x)
+        np.sqrt(x)
+        return x 
+    
+    dvm = np.nanmean(data)
+    dvsd = pop_sd(data)
+    s = (data - dvm) / dvsd
+    distr = ECDF(s)
+
+    return np.array(list(map(lambda x: -2 * np.log(distr(x)), s)))
+
+def calculate_covariances(data_matrix):
+    transformed_matrix = pd.DataFrame(list(map(lambda x: transform_data(data_matrix.loc[x,:].to_numpy()), data_matrix.index))).transpose()
+    
+    return transformed_matrix.cov()
+
 
--- R/merge_p.r
+++ R/merge_p.r
@@ -3,6 +3,7 @@
 #' @param scores Either a list of p-values or a matrix where each column is a test.
 #' @param method Method to merge p-values. See 'methods' section below.
 #' @param scores_direction Either a vector of log2 transformed fold-change values or a matrix where each column is a test. 
+#' Only datasets with a known directional relationship should be provided.
 #' @param expected_direction  A numerical vector of +1 or -1 values corresponding to the expected
 #'   directional relationship between columns in scores_direction.
 #'
@@ -51,13 +52,16 @@ merge_p_values <- function(scores, method = "Fisher", scores_direction = NULL,
     
     
     # Validation on scores_direction and expected_direction
+    if (!is.null(scores_direction) && is.null(expected_direction)) stop("The expected_direction parameter must be provided")
+    if (is.null(scores_direction) && !is.null(expected_direction)) stop("The scores_direction parameter must be provided")
     if (!is.null(scores_direction) && !is.null(expected_direction)){
+        if (!(is.numeric(expected_direction) && is.vector(expected_direction))) stop("expected_direction must be a numeric vector")
+        if (!is.null(expected_direction) && any(!expected_direction %in% c(1,-1))) stop("expected_direction must contain 1 or -1 values")
         if (is.vector(scores_direction) && is.matrix(scores)) stop ("scores and scores_direction must be the same data type")
         if (is.matrix(scores_direction) && is.vector(scores)) stop ("scores and scores_direction must be the same data type")
         if (!(is.vector(scores_direction) || is.matrix(scores_direction))) stop("scores_direction must be a matrix or vector")
         if (any(is.na(scores_direction))) stop("scores_direction may not contain missing values")
         if (!is.numeric(scores_direction)) stop("scores_direction must be numeric")
-        if (!(is.numeric(expected_direction) && is.vector(expected_direction))) stop("expected_direction must be a numeric vector")
         
         if (is.matrix(scores_direction) && is.matrix(scores)){
             if (any(!rownames(scores_direction) %in% rownames(scores))) stop ("scores_direction gene names must match scores genes")
@@ -110,21 +114,22 @@ merge_p_values <- function(scores, method = "Fisher", scores_direction = NULL,
     
     # If scores is a matrix with multiple columns, apply the following methods
     scores <- apply(scores, c(1,2), function(x) ifelse (x == 0, 1e-300, x))
+    
+    if (method == "Fisher"){
+        fisher_merged <- c()
+        for(i in 1:length(scores[,1])) {
+            p_fisher <- stats::pchisq(fishersMethod(scores[i,], scores_direction[i,],expected_direction),
+                                      2*length(scores[i,]), lower.tail = FALSE)
+            fisher_merged <- c(fisher_merged,p_fisher)
+        }
+        names(fisher_merged) <- rownames(scores)
+        return(fisher_merged)
+    }
     if (method == "Brown") {
         cov_matrix <- calculateCovariances(t(scores))
         brown_merged <- brownsMethod(scores, cov_matrix = cov_matrix, scores_direction = scores_direction,
                                      expected_direction = expected_direction)
         return(brown_merged)
-    }
-    if (method == "Fisher"){
-        fisher_merged <- c()
-        for(i in 1:length(scores[,1])) {
-            p_fisher <- stats::pchisq(fishersMethod(scores[i,], scores_direction[i,],expected_direction),
-                                         2*length(scores[i,]), lower.tail = FALSE)
-            fisher_merged <- c(fisher_merged,p_fisher)
-        }
-        names(fisher_merged) <- rownames(scores)
-        return(fisher_merged)
     }
     if (method == "Stouffer"){
         stouffer_merged <- c()
@@ -139,50 +144,8 @@ merge_p_values <- function(scores, method = "Fisher", scores_direction = NULL,
         strube_merged <- strubesMethod(scores,scores_direction,expected_direction)
         return(strube_merged)
     }
-    
 }
 
-stouffersMethod <- function (p_values, scores_direction,expected_direction){
-    k = length(p_values)
-    if (!is.null(scores_direction) && !is.null(expected_direction)){
-        directionality <- expected_direction * scores_direction/abs(scores_direction)
-        p_values_directional <- p_values[names(p_values) %in% names(scores_direction)]
-        z_directional <- abs(sum(stats::qnorm(p_values_directional/2)*directionality))
-        
-        if (length(p_values_directional) != k){
-            p_values_nondirectional <- p_values[!names(p_values) %in% names(scores_direction)]
-            z_nondirectional <- abs(sum(stats::qnorm(p_values_nondirectional/2)))
-        } else {
-            z_nondirectional <- 0
-        }
-        z_values <- c(z_directional, z_nondirectional)
-    } else {
-        z_values <- stats::qnorm(p_values/2)
-    }
-    z <- sum(z_values)/sqrt(k)
-    z
-}
-
-strubesMethod <- function (p_values, scores_direction, expected_direction){
-    #acquiring the unadjusted z-value from Stouffer's method.
-    stouffer_z <- c()
-    for(i in 1:length(p_values[,1])){
-        stouffer_z <- c(stouffer_z,stouffersMethod(p_values[i,], scores_direction[i,],expected_direction))
-    }
-    names(stouffer_z) <- rownames(p_values)
-    k = length(p_values[1,])
- 
-    #correlation matrix
-    cor_mtx <- stats::cor(p_values, use = "complete.obs")
-    cor_mtx[is.na(cor_mtx)] <- 0
-    cor_mtx <- abs(cor_mtx)
-    
-    #adjusted p-value
-    adjusted_z <- stouffer_z * sqrt(k) / sqrt(sum(cor_mtx))
-    p_strube <- 2*stats::pnorm(-1*abs(adjusted_z))
-    names(p_strube) <- rownames(p_values)
-    p_strube
-}
 
 fishersMethod <- function(p_values, scores_direction, expected_direction) {
     if (!is.null(scores_direction) && !is.null(expected_direction)){
@@ -233,13 +196,13 @@ brownsMethod <- function(p_values, data_matrix = NULL, cov_matrix = NULL, scores
         message("Both data_matrix and cov_matrix were supplied. Ignoring data_matrix")
     }
     if (missing(cov_matrix)) cov_matrix <- calculateCovariances(data_matrix)
-
+    
     N <- ncol(cov_matrix)
     expected <- 2 * N
     cov_sum <- 2 * sum(cov_matrix[lower.tri(cov_matrix, diag=FALSE)])
     var <- (4 * N) + cov_sum
     sf <- var / (2 * expected)
-
+    
     df <- (2 * expected^2) / var
     if (df > 2 * N) {
         df <- 2 * N
@@ -258,6 +221,51 @@ brownsMethod <- function(p_values, data_matrix = NULL, cov_matrix = NULL, scores
     names(p_brown) <- rownames(p_values)
     p_brown
 }
+
+
+stouffersMethod <- function (p_values, scores_direction,expected_direction){
+    k = length(p_values)
+    if (!is.null(scores_direction) && !is.null(expected_direction)){
+        directionality <- expected_direction * scores_direction/abs(scores_direction)
+        p_values_directional <- p_values[names(p_values) %in% names(scores_direction)]
+        z_directional <- abs(sum(stats::qnorm(p_values_directional/2)*directionality))
+        
+        if (length(p_values_directional) != k){
+            p_values_nondirectional <- p_values[!names(p_values) %in% names(scores_direction)]
+            z_nondirectional <- abs(sum(stats::qnorm(p_values_nondirectional/2)))
+        } else {
+            z_nondirectional <- 0
+        }
+        z_values <- c(z_directional, z_nondirectional)
+    } else {
+        z_values <- stats::qnorm(p_values/2)
+    }
+    z <- sum(z_values)/sqrt(k)
+    z
+}
+
+
+strubesMethod <- function (p_values, scores_direction, expected_direction){
+    #acquiring the unadjusted z-value from Stouffer's method.
+    stouffer_z <- c()
+    for(i in 1:length(p_values[,1])){
+        stouffer_z <- c(stouffer_z,stouffersMethod(p_values[i,], scores_direction[i,],expected_direction))
+    }
+    names(stouffer_z) <- rownames(p_values)
+    k = length(p_values[1,])
+ 
+    #correlation matrix
+    cor_mtx <- stats::cor(p_values, use = "complete.obs")
+    cor_mtx[is.na(cor_mtx)] <- 0
+    cor_mtx <- abs(cor_mtx)
+    
+    #adjusted p-value
+    adjusted_z <- stouffer_z * sqrt(k) / sqrt(sum(cor_mtx))
+    p_strube <- 2*stats::pnorm(-1*abs(adjusted_z))
+    names(p_strube) <- rownames(p_values)
+    p_strube
+}
+
 
 transformData <- function(dat) {
     # If all values in dat are the same (equal to y), return dat. The covariance

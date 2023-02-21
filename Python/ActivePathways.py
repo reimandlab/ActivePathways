f39b68f7328acc608fd84a7549d10f584a88135d
MSlobodyanyuk <michael.slobody@gmail.com>
2023-02-21 14:53:22 -0500

    Updated validation errors

--- Python/ActivePathways.py
+++ Python/ActivePathways.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import numpy as np
+from sys import exit
 import scipy.stats as stats
 import statsmodels.stats.multitest as multi
 import matplotlib as mpl
 import matplotlib.pyplot 
+from gmt import *
+from statistical_tests import *
+from cytoscape import *
+from merge_p import *
 
-def ActivePathways(scores,gmt, background = make_background(read_gmt(gmt).genes_dict),
+def ActivePathways(scores,gmt, background = None,
                    geneset_filter = [5,1000], cutoff = 0.1, significant = 0.05,
                    merge_method = ["Brown","Fisher","Stouffer","Strube"],
                    correction_method = ["holm", "hommel","bonferroni", "fdr_bh", "fdr_by", "none"],
@@ -42,7 +47,7 @@ def ActivePathways(scores,gmt, background = make_background(read_gmt(gmt).genes_
         if type(expected_direction) != np.ndarray and type(expected_direction) != pd.Series:
             print("expected_direction must be a numeric type array")
             exit()
-        if ~np.all(np.isin(expected_direction,[1,-1,0])):
+        if not np.all(np.isin(expected_direction,[1,-1,0])):
             print("expected_direction must contain the values: 1, -1 or 0")
             exit()
         if type(scores_direction) != pd.DataFrame:
@@ -56,93 +61,93 @@ def ActivePathways(scores,gmt, background = make_background(read_gmt(gmt).genes_
         except:
             print("scores_direction must be numeric")
             exit()
-        if np.any(~np.isin(scores_direction.index, scores.index)) or np.any(~np.isin(scores_direction.columns, scores.columns)):
+        if np.any(not np.isin(scores_direction.index, scores.index)) or np.any(not np.isin(scores_direction.columns, scores.columns)):
             print("scores_direction index and columns must match scores index and columns")
             exit()
         if scores_direction.shape[1] != len(expected_direction):
             print("expected_direction should have the same number of entries as columns in scores_direction")
             exit()    
-        if np.any(np.isin(expected_direction,0)) and ~np.all(scores_direction.loc[:,np.isin(expected_direction,0)] == 0):
+        if np.any(np.isin(expected_direction,0)) and not np.all(scores_direction.loc[:,np.isin(expected_direction,0)] == 0):
             print("scores_direction entries must be set to 0's for columns that do not contain directional information")
             exit()
 
     # cutoff and significant
-    if len(cutoff) != 1 or len(significant) != 1:
-        exit()
-    if ~isinstance(cutoff,(int,float)):
+    if not isinstance(cutoff,(int,float)):
         print("cutoff must be numeric")
         exit()
-    if ~isinstance(significant,(int,float)):
+    if not isinstance(significant,(int,float)):
         print("significant must be numeric")
         exit()
     if cutoff < 0 or cutoff > 1:
-        print("cutoff must be a value in [0,1]')
+        print("cutoff must be a value in [0,1]")
         exit()
     if significant < 0 or significant > 1:
-        print("significant must be a value in [0,1]')
+        print("significant must be a value in [0,1]")
         exit()
     
     # gmt
-    if ~is_gmt(gmt):
+    if background == None:
+        background = make_background(read_gmt(gmt).genes_dict)
+    if not is_gmt(gmt):
         gmt = read_gmt(gmt)
     if type(background) == list:
         background = np.array(background)
     if type(background) != np.ndarray and type(background) != pd.Series:
         print("background must be an array")
         exit()
-    if ~np.all(list(map(lambda x: isinstance(x,str),background))):
+    if not np.all(list(map(lambda x: isinstance(x,str),background))):
         print("background must contain string values")
         exit()
     
     # geneset_filter
     if geneset_filter != None:
         if type(geneset_filter) == list:
-              geneset_filter = np.array(geneset_filter)
+            geneset_filter = np.array(geneset_filter)
         if type(geneset_filter) != np.ndarray and type(geneset_filter) != pd.Series:
-              print("geneset_filter must be an array")
-              exit()
-        if ~np.all(list(map(lambda x: isinstance(x,int), geneset_filter))):
-              print("geneset_filter must be numeric")
-              exit()
+            print("geneset_filter must be an array")
+            exit()
+        if not np.all(list(map(lambda x: isinstance(x,(int, np.integer)), geneset_filter))):
+            print("geneset_filter must be numeric")
+            exit()
         if len(geneset_filter) != 2:
-              print("geneset_filter must be length 2")
-              exit()
+            print("geneset_filter must be length 2")
+            exit()
         if np.any([x < 0 for x in geneset_filter]):
-              print("geneset_filter limits must be positive")
-              exit()
+            print("geneset_filter limits must be positive")
+            exit()
               
     # custom_colors
     if custom_colors != None and type(scores) == pd.DataFrame:
         if type(custom_colors) == list:
-              custom_colors = np.array(custom_colors)
+            custom_colors = np.array(custom_colors)
         if type(custom_colors) != np.ndarray and type(custom_colors) != pd.Series:
-              print("custom_colors must be an array")
-              exit()
-        if ~np.all(list(map(lambda x: isinstance(x,str),custom_colors))):
-              print("custom_colors must contain string values")
-              exit()
+            print("custom_colors must be an array")
+            exit()
+        if not np.all(list(map(lambda x: isinstance(x,str),custom_colors))):
+            print("custom_colors must contain string values")
+            exit()
         if len(scores.columns) != len(custom_colors):
-              print("incorrect number of colors is provided")
-              exit()
+            print("incorrect number of colors is provided")
+            exit()
         if color_palette != None:
-              print("Both custom_colors and color_palette are provided. Specify only one of these parameters for node coloring.")
-              exit()
+            print("Both custom_colors and color_palette are provided. Specify only one of these parameters for node coloring.")
+            exit()
         if type(custom_colors) == pd.Series:
-              if ~np.all(np.isin(custom_colors.index,scores.columns)):
-                  print("The custom_colors index names should match the scores column names")
-                  exit()
+            if not np.all(np.isin(custom_colors.index,scores.columns)):
+                print("The custom_colors index names should match the scores column names")
+                exit()
     
     # color_palette
     if color_palette != None:
         if color_palette not in mpl.pyplot.colormaps():
-              print("The color palette must be one of the colormaps in the matplotlib package")
-              exit()
+            print("The color palette must be one of the colormaps in the matplotlib package")
+            exit()
               
     # color_integrated_only
-    if ~isinstance(color_integrated_only,str):
+    if not isinstance(color_integrated_only,str):
         print("color_integrated_only must be provided as a string")
         exit()
-    if len(color_integrated_only) != 1:
+    if len([color_integrated_only]) != 1:
         print("only a single color must be specified")
         exit()
     
@@ -166,20 +171,20 @@ def ActivePathways(scores,gmt, background = make_background(read_gmt(gmt).genes_
         print("{} rows were removed from scores because they are not found in the background".format(orig_length - len(scores.index)))
     
   # Filter the GMT by genes of interest and number of genes in gene sets
-    if ~np.all(np.isin(background, make_background(gmt.genes_dict))):
-        # original number of gene sets
-        origin_num_gene_sets = len(gmt.genes_dict.keys())
-        # filter the gmt dictionary
-        for key, gene_list in gmt.genes_dict.items():
-            common_genes = np.intersect1d(gene_list, background)
+    #if not np.all(np.isin(background, make_background(gmt.genes_dict))):
+    # original number of gene sets
+    origin_num_gene_sets = len(gmt.genes_dict.keys())
+    # filter the gmt dictionary
+    for key, gene_list in list(gmt.genes_dict.items()):
+        common_genes = np.intersect1d(gene_list, background)
 
-            # remove if there are no genes, or if the number of genes is outside parameters
-            if len(common_genes) == 0 or len(common_genes) < geneset_filter[0] or len(common_genes) > geneset_filter[1]:
-                gmt.genes_dict.pop(key)
-                gmt.name_dict.pop(key)
+        # remove if there are no genes, or if the number of genes is outside parameters
+        if len(common_genes) == 0 or len(common_genes) < geneset_filter[0] or len(common_genes) > geneset_filter[1]:
+            gmt.genes_dict.pop(key)
+            gmt.name_dict.pop(key)
 
-            else:
-                gmt.genes_dict[key] = common_genes
+        else:
+            gmt.genes_dict[key] = common_genes
 
     # count number of pathways that passed
     if len(gmt.genes_dict.keys()) == 0:
@@ -271,9 +276,9 @@ def columnSignificance(scores, gmt, background, cutoff, significant, correction_
         ev = ev_names[~df.iloc[x,2:].isnull()]
         if len(ev) == 0:
             if pvals[x] <= significant:
-              ev = 'combined'
+                ev = 'combined'
             else:
-              ev = 'none'
+                ev = 'none'
         return ev
     evidence = list(map(lambda x: set_evidence(x), range(len(df.index))))
     df.loc[:,'evidence'] = evidence
